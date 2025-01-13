7f32457e62d670dcfa15b68311920961c56e83e1
Mattis3858 <109306011@nccu.edu.tw>
2025-01-13 14:52:47 +0800

    change the url to .env file path

--- src/apis/api_urls.js
+++ src/apis/api_urls.js
@@ -1,3 +1,2 @@
 // export const base_url = "http://127.0.0.1:8000/";
-export const base_url =
-  process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000/";
+export const base_url = process.env.REACT_APP_BASE_URL;
